fc657b751ae8224e133f5eef23865a8616f27501
paradoxmkt <paradoox.pessoalyt@gmail.com>
2025-09-15 21:12:28 -0300

    RBX.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import AnimatedBackground from '@/components/common/AnimatedBackground';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Roblox Quiz Mania',
@@ -26,6 +27,23 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "68c8aa43fd29f31c0e6a31f0";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></Script>
       </head>
       <body className="font-body antialiased">
         <AnimatedBackground />
